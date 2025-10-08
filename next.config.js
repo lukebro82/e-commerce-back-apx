4a3e586527af8720054403eb82369c2b7eb0c961
Lucas <lucas.82@hotmail.com.ar>
2025-10-08 11:57:31 -0300

    last commit

--- next.config.js
+++ next.config.js
@@ -2,22 +2,19 @@ module.exports = {
   async headers() {
     return [
       {
-        // Aplica estos headers a todas las rutas API
+        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3001",
-          }, // Cambia al puerto correcto
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
